e1e0f4bf0c3f4160cae1c3c64d0f5f8bb9d3b1a8
Auralym <aurimlilim@gmail.com>
2025-09-30 21:22:16 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -31,6 +31,30 @@ document.addEventListener("DOMContentLoaded", async () => {
   );
 
 
+  /* Affiliate
+  ===================================================================== */
+  let prompts = await charadex.initialize.page(null, charadex.page.index.affiliates, (arr) => {
+
+      // Splice the silly little array
+      let sliceAmount = charadex.page.index.affiliates.amount || 4;
+      arr.splice(sliceAmount, arr.length);
+
+    }, (data) => {
+
+      // Add the silly little prompt stuff here too
+      $('.cd-prompt-background').each(function(i) {
+        const element = $(this);
+        const image = data.array[i]?.image;
+        element.attr('style', `background-image: url(${image})`);
+      });
+      
+    }
+    
+  );
+
+
+  
+
   /* Staff
   ===================================================================== */
   let staff = await charadex.initialize.page(null, charadex.page.index.staff, (arr) => {
