e8dce3f745e5a22f17148ef44caec2feb3f71872
Auralym <aurimlilim@gmail.com>
2025-09-30 22:45:47 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -66,17 +66,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
 
-  /* Designs
-  ===================================================================== */
-  let designs = await charadex.initialize.page(null, charadex.page.index.designs, (arr) => {
-    
-    // Splice the silly little array
-    let sliceAmount = charadex.page.index.designs.amount || 6;
-    arr.splice(sliceAmount, arr.length);
-
-  });
-
-
   /* Load Page
   ===================================================================== */
   charadex.tools.loadPage('.softload', 500);
