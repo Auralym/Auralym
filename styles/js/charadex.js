bea4294aa07ed33904b9483e02f2cc85deab48f1
afe / aerin <57576856+aferael@users.noreply.github.com>
2026-09-18 16:24:21 -0700

    Update charadex.js

--- styles/js/charadex.js
+++ styles/js/charadex.js
@@ -38,6 +38,7 @@ charadex.initialize.page = async (dataArr, config, dataCallback, listCallback, c
     if (folders) folders(entry, config.fauxFolder.folderProperty); // If folders, add folder info
     if (entry.rarity) entry.raritybadge = `<span class="badge badge-${charadex.tools.scrub(entry.rarity)}">${entry.rarity}</span>`; // Adds a rarity badge
     if (entry.tendency) entry.tendencybadge = `<span class="badge badge-${charadex.tools.scrub(entry.tendency)}">${entry.tendency}</span>`; // Adds a tendecy badge
+    if (entry.subspecies) entry.subspeciesbadge = `<span class="badge badge-${charadex.tools.scrub(entry.subspecies)}">${entry.subspecies}</span>`; // Adds a tendecy badge
     if (entry.progress) entry.progress = `<div class="progress progress-track" style="width:${charadex.tools.scrub(entry.progress)}%"></div>`; // Makes progress bar work
     if (entry.mprogress) entry.mprogress = `<div class="progress progress-track" style="width:${charadex.tools.scrub(entry.mprogress)}%; height: 10px;"></div>`; // Might
     if (entry.fprogress) entry.fprogress = `<div class="progress progress-track" style="width:${charadex.tools.scrub(entry.fprogress)}%; height: 10px;"></div>`; // Finesse
